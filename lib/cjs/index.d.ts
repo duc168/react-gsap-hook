ff243560460967a3a5a427b7deac81a6bd2747cd
victor <victor.tran.august@gmail.com>
2024-01-12 02:58:52 +0700

    feat: export utils

--- lib/cjs/index.d.ts
+++ lib/cjs/index.d.ts
@@ -1,4 +1,17 @@
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+
+declare const tweenClear: (anim: gsap.core.Tween) => void;
+declare const timelineClear: (anim: gsap.core.Timeline) => void;
+
+declare const gsapUtil: {
+    gsap: typeof globalThis.gsap;
+    ScrollTrigger: typeof ScrollTrigger;
+    goToSection: (section: Element, anim?: any) => void;
+    scrollToX: (element: Element) => gsap.core.Tween;
+    scrollToY: (element: Element, autoKill?: boolean) => gsap.core.Tween;
+};
+
 declare const useReactGsap: (inView: boolean, animClassName: string, fromTween: gsap.TweenVars, toTween: gsap.TweenVars, isReversed?: boolean) => void;
 
-export { useReactGsap };
+export { gsapUtil, timelineClear, tweenClear, useReactGsap };
 //# sourceMappingURL=index.d.ts.map
--- packages/react-gsap-hook/src/index.tsx
+++ packages/react-gsap-hook/src/index.tsx
@@ -1,4 +1,8 @@
 import useReactGSAP from "./useReactGsapHook"
+import { timelineClear, tweenClear } from "./utils/animation";
+import gsapUtil from "./utils/gsap";
 
 // export type Props = UploaderProps
 export const useReactGsap = useReactGSAP
+
+export { timelineClear, tweenClear, gsapUtil };
